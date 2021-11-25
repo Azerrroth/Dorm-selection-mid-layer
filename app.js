9361af5c9c07d37fde783991f54bcc378bd11751
Azerrroth <732814312@qq.com>
2021-11-26 01:26:26 +0800

    Update: configs

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var proxy = require('express-http-proxy');
+const cors = require('cors');
+
 
 var router = require('./routes/api')
 
@@ -12,14 +15,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+const urls = {
+  userPath: 'http://sincos.vip',
+  dormPath: 'http://sincos.vip',
+  orderPath: 'http://sincos.vip',
+  loginPath: 'http://sincos.vip',
+  registerPath: 'http://sincos.vip',
+  tokenPath: 'http://sincos.vip',
+  testPath: 'http://sincos.vip',
+}
+
+app.use('/api/v1/user/*', proxy(urls.userPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+app.use('/api/v1/dorm/*', proxy(urls.dormPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+app.use('/api/v1/order/*', proxy(urls.orderPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+
+app.use('/api/v1/login', proxy(urls.loginPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+app.use('/api/v1/register', proxy(urls.registerPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+app.use('/api/v1/token', proxy(urls.tokenPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+app.use('/test', proxy(urls.testPath, { proxyReqPathResolver: (req, res) => { return req.baseUrl } }))
+
+
 
 var history = require('connect-history-api-fallback');
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -28,16 +50,14 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api/*', router)
 
-// TODO: Waiting for finished https://blog.csdn.net/astonishqft/article/details/82762354
-// 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET api listing. */
-router.get('/', function(req, res, next) {
+router.get('/v1/*', function (req, res, next) {
+  console.log(req);
+  res.send('respond with a resource');
+});
+
+
+/* POST api listing. */
+router.post('/*', function (req, res, next) {
+  console.log(req)
   res.send('respond with a resource');
 });
 
